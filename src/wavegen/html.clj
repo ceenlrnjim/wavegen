07494327e5da9e734c723c05de6f1948c528a2ee
James Kirkwood <kirkwojl@hotmail.com>
2012-02-14 12:10:19 -0500

    work on styling

--- src/wavegen/html.clj
+++ src/wavegen/html.clj
@@ -5,6 +5,30 @@
 (def formatter (java.text.DecimalFormat. "#0.0#"))
 
 ; TODO: move HTML snippets to resource file and use reader to load separate from code
+(def HEADER_LINE_1_HEADER "<tr class='header'><td colspan='5'></td><td colspan='3'>Weightings</td>")
+(def HEADER_PRODUCT_NAME "<td colspan='3'>~NAME~</td>")
+(def HEADER_LINE_1_TERMINATOR "</tr>\n")
+
+(def HEADER_LINE_2_HEADER "<tr class='header'><td class='linenumlabel'><td class='categorylabel'>Category</td><td class='subcategorylabel'>Sub-category</td><td class='reqtlabel'>Requirement</td><td class='criterialabel'>Evaluation Criteria</td><td class='reqtwtlabel'>Reqt</td><td class='subcatwtlabel'>Sub-cat</td><td class='catwtlabel'>Category</td>")
+(def HEADER_LINE_2_PRODUCT_HEADER "<td class='scorelabel'>Score</td><td class='noteslabel'>Notes</td><td class='wtscorelabel'>Wgt Score</td>")
+(def HEADER_LINE_2_TERMINATOR "</tr>\n")
+
+(def CATEGORY_HEADER "<tr class='category'><td class='linenum'>~LINE~</td><td colspan='4' class='category-name'>~NAME~</td><td/><td/><td class='category-weight'>~WEIGHT~</td>")
+(def CATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
+(def CATEGORY_TERMINATOR "</tr>\n")
+
+(def SUBCATEGORY_HEADER "<tr class='subcategory'><td class='linenum'>~LINE~</td><td/><td colspan='3' class='subcategory-name'>~NAME~</td><td/><td class='subcategory-weight'>~WEIGHT~</td><td/>")
+(def SUBCATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
+(def SUBCATEGORY_TERMINATOR "</tr>\n")
+
+(def REQT_HEADER "<tr class='requirement'><td class='linenum'>~LINE~</td><td/><td/><td>~DESC~</td><td class='criteria'>~CRITERIA~</td><td>~WEIGHT~</td><td/><td/>")
+(def REQT_PRODUCT_SCORE "<td>~RAW~</td><td/><td>~SCORE~</td>")
+(def REQT_TERMINATOR "</tr>\n")
+
+(def TOTAL_HEADER "<tr class='total'><td/><td/><td/><td/><td colspan='4'>Final Score:</td>")
+(def TOTAL_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
+(def TOTAL_TERMINATOR "</tr>\n")
+
 ;
 ; HTML generation functions ------------------------------------------------------------------------
 ;
@@ -19,13 +43,6 @@
   [output wave]
   (.append output "<html><head><link rel='stylesheet' href='wave.css'></head><body><table><tbody>"))
 
-(def HEADER_LINE_1_HEADER "<tr class='header'><td colspan='5'></td><td colspan='3'>Weightings</td>")
-(def HEADER_PRODUCT_NAME "<td colspan='3'>~NAME~</td>")
-(def HEADER_LINE_1_TERMINATOR "</tr>\n")
-
-(def HEADER_LINE_2_HEADER "<tr class='header'><td/><td>Category</td><td>Sub-category</td><td>Requirement</td><td>Evaluation Criteria</td><td>Reqt</td><td>Sub-cat</td><td>Category</td>")
-(def HEADER_LINE_2_PRODUCT_HEADER "<td>Score</td><td>Notes</td><td>Wgt Score</td>")
-(def HEADER_LINE_2_TERMINATOR "</tr>\n")
 
 ; TODO: can I create a protocol for these things that I can use for CSV, etc.?
 (defn- header
@@ -41,9 +58,6 @@
     (.append output HEADER_LINE_2_PRODUCT_HEADER))
   (.append output HEADER_LINE_2_TERMINATOR))
 
-(def CATEGORY_HEADER "<tr class='category'><td class='linenum'>~LINE~</td><td colspan='4' class='category-name'>~NAME~</td><td/><td/><td class='category-weight'>~WEIGHT~</td>")
-(def CATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def CATEGORY_TERMINATOR "</tr>\n")
 
 (defn- category
   "scores is a map of product id to weighted score"
@@ -53,10 +67,6 @@
     (.append output (replace-tokens CATEGORY_PRODUCT_SCORE "~SCORE~" (.format formatter (get scores pid)))))
   (.append output CATEGORY_TERMINATOR))
 
-
-(def SUBCATEGORY_HEADER "<tr class='subcategory'><td class='linenum'>~LINE~</td><td/><td colspan='3' class='subcategory-name'>~NAME~</td><td/><td class='subcategory-weight'>~WEIGHT~</td><td/>")
-(def SUBCATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def SUBCATEGORY_TERMINATOR "</tr>\n")
 ; TODO: very similar to category - refactor
 (defn- subcategory
   [output cat sub weight scores prod-ids linenum]
@@ -65,19 +75,12 @@
     (.append output (replace-tokens SUBCATEGORY_PRODUCT_SCORE "~SCORE~" (.format formatter (get scores pid)))))
   (.append output SUBCATEGORY_TERMINATOR))
 
-(def REQT_HEADER "<tr class='requirement'><td class='linenum'>~LINE~</td><td/><td/><td>~DESC~</td><td class='criteria'>~CRITERIA~</td><td>~WEIGHT~</td><td/><td/>")
-(def REQT_PRODUCT_SCORE "<td>~RAW~</td><td/><td>~SCORE~</td>")
-(def REQT_TERMINATOR "</tr>\n")
 (defn- requirement
   [output wave r prod-ids linenum]
   (.append output (replace-tokens REQT_HEADER "~LINE~" linenum "~DESC~" (:desc r) "~CRITERIA~" (:scores r) "~WEIGHT~" (.format formatter (reqt-weight wave r))))
   (doseq [pid prod-ids]
     (.append output (replace-tokens REQT_PRODUCT_SCORE "~RAW~" (get-score wave pid (:id r)) "~SCORE~" (.format formatter (weighted-score wave (:id r) pid)))))
   (.append output REQT_TERMINATOR))
-
-(def TOTAL_HEADER "<tr class='total'><td/><td/><td/><td/><td colspan='4'>Final Score:</td>")
-(def TOTAL_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def TOTAL_TERMINATOR "</tr>\n")
 
 (defn- totals
   [output scores prod-ids]
