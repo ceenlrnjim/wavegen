55e9d510481bfacd084f88cb283b709d7a4e6a97
James Kirkwood <kirkwojl@hotmail.com>
2013-03-22 20:22:13 -0400

    initial restructure of html processing

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wavegen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.apache.poi/poi "3.7"]
                  [log4j/log4j "1.2.16"]]
--- src/wavegen/html.clj
+++ src/wavegen/html.clj
@@ -2,120 +2,71 @@
   (:use [wavegen.core])
   (:use [wavegen.aggr]))
 
-(def formatter (java.text.DecimalFormat. "#0.0#"))
-(defn decfmt 
-  "Returns the specific number as a standard format string"
-  [d] (.format formatter d))
+(defn branch?
+  [n]
+  (contains? #{:wave :category :subcategory} (:type n)))
 
-; TODO: move HTML snippets to resource file and use reader to load separate from code
-(def HEADER_LINE_1_HEADER "<tr class='header'><td colspan='5'></td><td colspan='3'>Weightings</td>")
-(def HEADER_PRODUCT_NAME "<td colspan='3'>~NAME~</td>")
-(def HEADER_LINE_1_TERMINATOR "</tr>\n")
+(defmulti node-children :type)
+(defmethod node-children :wave [{:keys [wave]}]
+  (map #(assoc {} :wave wave 
+                  :category % 
+                  :type :category) 
+        (categories wave)))
 
-(def HEADER_LINE_2_HEADER "<tr class='header'><td class='linenumlabel'></td><td class='categorylabel'>Category</td><td class='subcategorylabel'>Sub-category</td><td class='reqtlabel'>Requirement</td><td class='criterialabel'>Evaluation Criteria</td><td class='reqtwtlabel'>Reqt</td><td class='subcatwtlabel'>Sub-cat</td><td class='catwtlabel'>Category</td>")
-(def HEADER_LINE_2_PRODUCT_HEADER "<td class='scorelabel'>Score</td><td class='noteslabel'>Notes</td><td class='wtscorelabel'>Wgt Score</td>")
-(def HEADER_LINE_2_TERMINATOR "</tr>\n")
+(defmethod node-children :category [{:keys [wave category]}]
+  (map #(assoc {} :wave wave 
+                  :category category 
+                  :type :subcategory 
+                  :subcategory %) 
+       (subcategories wave category)))
 
-(def CATEGORY_HEADER "<tr class='category'><td class='linenum'>~LINE~</td><td colspan='4' class='category-name'>~NAME~</td><td/><td/><td class='category-weight'>~WEIGHT~</td>")
-(def CATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def CATEGORY_TERMINATOR "</tr>\n")
+(defmethod node-children :subcategory [{:keys [wave category subcategory]}]
+  (map #(assoc {} :wave wave
+                  :category category
+                  :subcategory subcategory
+                  :requirement %
+                  :type :requirement)
+       (requirements wave category subcategory)))
 
-(def SUBCATEGORY_HEADER "<tr class='subcategory'><td class='linenum'>~LINE~</td><td/><td colspan='3' class='subcategory-name'>~NAME~</td><td/><td class='subcategory-weight'>~WEIGHT~</td><td/>")
-(def SUBCATEGORY_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def SUBCATEGORY_TERMINATOR "</tr>\n")
+; returns a sequence of streams for the wave hierarchy
+; TODO: how to address before/after open/close tags and lines
+(defmulti render-before :type)
+(defmethod render-before :wave [node]
+  ["before wave"])
+(defmethod render-before :category [node]
+  ["before category"])
+(defmethod render-before :subcategory [node]
+  ["before subcat"])
+(defmethod render-before :requirement [node]
+  ["before reqt"])
+(defmethod render-before :totals [node]
+  ["before totals"])
 
-(def REQT_HEADER "<tr class='requirement'><td class='linenum'>~LINE~</td><td/><td/><td>~DESC~</td><td class='criteria'>~CRITERIA~</td><td>~WEIGHT~</td><td/><td/>")
-(def REQT_PRODUCT_SCORE "<td>~RAW~</td><td/><td>~SCORE~</td>")
-(def REQT_TERMINATOR "</tr>\n")
+(defmulti render-after :type)
+(defmethod render-after :wave [node]
+  ["after wave"])
+(defmethod render-after :category [node]
+  ["after category"])
+(defmethod render-after :subcategory [node]
+  ["after subcat"])
+(defmethod render-after :requirement [node]
+  ["after reqt"])
+(defmethod render-after :totals [node]
+  ["after totals"])
 
-(def TOTAL_HEADER "<tr class='total'><td/><td/><td/><td/><td colspan='4'>Final Score:</td>")
-(def TOTAL_PRODUCT_SCORE "<td/><td/><td>~SCORE~</td>")
-(def TOTAL_TERMINATOR "</tr>\n")
+(defn render-tree
+  "Returns a sequence of strings for the tree"
+  [node]
+  (let [b (render-before node)
+        a (render-after node)]
+    (if 
+      (branch? node) 
+      (flatten [b (map render-tree (node-children node)) a])
+      [b a])))
 
-;
-; HTML generation functions ------------------------------------------------------------------------
-;
-(defn- replace-tokens
-  "Replaces each of the tokens in the token/value pairs with the corresponding value in the specified string"
-  [string & token-value-pairs]
-  (if (empty? token-value-pairs) string
-    (recur (.replace string (str (first token-value-pairs)) (str (second token-value-pairs))) (rest (rest token-value-pairs)))))
-
-(defn- page
-  "renders the page level html"
-  [output wave]
-  (.append output "<html><head><link rel='stylesheet' href='wave.css'></head><body><table><tbody>"))
-
-
-; TODO: can I create a protocol for these things that I can use for CSV, etc.?
-(defn- header
-  "Generates an html header row and appends to the specified string builder"
-  [output wave prodkeys]
-  ; Total cells = 8 * (3 + # products)
-  (.append output HEADER_LINE_1_HEADER)
-  (doseq [pid prodkeys]
-    (.append output (.replace HEADER_PRODUCT_NAME "~NAME~" (:desc (get (:products wave) pid)))))
-  (.append output HEADER_LINE_1_TERMINATOR)
-  (.append output HEADER_LINE_2_HEADER)
-  (doseq [pid prodkeys]
-    (.append output HEADER_LINE_2_PRODUCT_HEADER))
-  (.append output HEADER_LINE_2_TERMINATOR))
-
-
-(defn- category
-  "scores is a map of product id to weighted score"
-  [output cat weight scores prod-ids linenum]
-  (.append output (replace-tokens CATEGORY_HEADER 
-                                  "~LINE~" linenum 
-                                  "~NAME~" cat 
-                                  "~WEIGHT~" (decfmt weight)))
-  (doseq [pid prod-ids]
-    (.append output (replace-tokens CATEGORY_PRODUCT_SCORE 
-                                    "~SCORE~" (decfmt (get scores pid)))))
-  (.append output CATEGORY_TERMINATOR))
-
-; TODO: very similar to category - refactor
-(defn- subcategory
-  [output cat sub weight scores prod-ids linenum]
-  (.append output (replace-tokens SUBCATEGORY_HEADER "~LINE~" linenum "~NAME~" sub "~WEIGHT~" (decfmt weight)))
-  (doseq [pid prod-ids]
-    (.append output (replace-tokens SUBCATEGORY_PRODUCT_SCORE "~SCORE~" (decfmt (get scores pid)))))
-  (.append output SUBCATEGORY_TERMINATOR))
-
-(defn- requirement
-  [output wave r prod-ids linenum]
-  (.append output (replace-tokens REQT_HEADER "~LINE~" linenum "~DESC~" (:desc r) "~CRITERIA~" (:scores r) "~WEIGHT~" (decfmt (reqt-weight wave r))))
-  (doseq [pid prod-ids]
-    (.append output (replace-tokens REQT_PRODUCT_SCORE "~RAW~" (get-score wave pid (:id r)) "~SCORE~" (decfmt (weighted-score wave (:id r) pid)))))
-  (.append output REQT_TERMINATOR))
-
-(defn- totals
-  [output scores prod-ids]
-  (.append output TOTAL_HEADER)
-  (doseq [pid prod-ids]
-    (.append output (replace-tokens TOTAL_PRODUCT_SCORE "~SCORE~" (decfmt (get scores pid)))))
-  (.append output TOTAL_TERMINATOR))
-
-(defn- end-page
-  [output wave]
-  (.append output "</tbody></table></body></html>"))
-
-; TODO: modify this to be a HOF that takes functions for rendering catgories, subcats, reqts, etc.
 (defn gen-html
-  "returns a string containing the HTML representation of the wave"
+  "returns a string containing the html representation of the specified wave"
   [wave]
-  (let [output (StringBuilder.)
-        prodlist (prod-keys wave)
-        counter (atom 0)]
-    (page output wave)
-    (header output wave prodlist)
-    (doseq [c (categories wave)]
-      (category output c (category-weight wave c) (category-scores wave c prodlist) prodlist (swap! counter inc))
-      (doseq [sc (subcategories wave c)]
-        (subcategory output c sc (subcat-weight wave c sc) (subcat-scores wave c sc prodlist) prodlist (swap! counter inc))
-        (doseq [r (requirements wave c sc)]
-          (requirement output wave r prodlist (swap! counter inc)))))
-    (totals output (total-scores wave prodlist) prodlist)
-    (end-page output wave)
-    (.toString output)))
-
+  (let [s (render-tree {:wave wave :type :wave})]
+    (println s)
+    (apply str s)))
