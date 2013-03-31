29924a5fe77d280b24300da117f84470f38470dd
James Kirkwood <kirkwojl@hotmail.com>
2013-03-31 09:42:28 -0400

    updated to reflect rename of rpg to relations

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [org.apache.poi/poi "3.7"]
                  [log4j/log4j "1.2.16"]]
-  :source-paths ["src" "/home/kirkwjl/projects/rpg/clj/src"]
+  :source-paths ["src" "/home/kirkwjl/projects/relations/clj/src"]
   :main wavegen.cli)
