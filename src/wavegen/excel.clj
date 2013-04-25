cfa5ccc5a3ed68ff26638456961012334ee2f032
Jim Kirkwood <jkirkwood@paychex.com>
2013-04-25 14:57:21 -0400

    cleaning up and simplifying functions - removing code

--- src/wavegen/excel.clj
+++ src/wavegen/excel.clj
@@ -7,58 +7,53 @@
 (def styles (atom {}))
 
 (defn color [r g b] (XSSFColor. (Color. (float (/ r 256.0)) (/ g 256.0) (/ b 256.0))))
-
-(defn ix-to-col
-  [c]
-  (str  (get "ABCDEFGHIJKLMNOPQRSTUVWXYZ" c)))
-
-(defn ix-to-row
-  [ix]
-  (inc ix))
+(defn ix2c [c] (str  (get "ABCDEFGHIJKLMNOPQRSTUVWXYZ" c)))
+(defn ix2r [ix] (inc ix))
+(defn ix-seq [s] (map-indexed vector s))
 
 (defn relwt-formula
   [col]
-  (fn [wave data col-ixs]
-    (let [cltr (ix-to-col (get col-ixs col))
-          total (str "SUM(" cltr "1:" cltr "1000)")]
-      (str cltr (ix-to-row (:rownum data)) "/" total))))
+  (fn [wave {rownum :rownum} col-ixs]
+    (let [cltr (ix2c (get col-ixs col))]
+      (str cltr (ix2r rownum) "/SUM(" cltr "1:" cltr "1000)"))))
 
 (defn row-ids
   [r pred]
   (into #{} (-> (rels/select r pred) (rels/col-seq :rownum))))
 
+(defn type-pred
+  [t data & ks]
+  (fn [r]
+    (and (= (:type r) t)
+      (every? (fn [k] (= (get r k) (get data k))) ks))))
+
+(defn items-formula
+  [col op rowtype & kvs]
+  (fn [wave data col-ixs]
+    (let [cltr (ix2c (get col-ixs col))
+          rowids (row-ids wave (apply type-pred rowtype data kvs))
+          cells (reduce #(str %1 "," %2) (map #(str cltr (ix2r %)) rowids))]
+      (str op "(" cells ")"))))
+
 (defn sum-subcat-formula
   [col]
-  (fn [wave data col-ixs]
-    (let [cltr (ix-to-col (get col-ixs col))
-          rowids (row-ids wave #(and (= (:type %) :subcategory) (= (:category %) (:category data))))
-          cells (reduce #(str %1 "," %2) (map #(str cltr (ix-to-row %)) rowids))]
-      (str "SUM(" cells ")"))))
+  (items-formula col "SUM" :subcategory :category))
 
 (defn sum-cat-formula
   [col]
-  (fn [wave data col-ixs]
-    (let [cltr (ix-to-col (get col-ixs col))
-          rowids (row-ids wave #(and (= (:type %) :category)))
-          cells (reduce #(str %1 "," %2) (map #(str cltr (ix-to-row %)) rowids))]
-      (str "SUM(" cells ")"))))
+  (items-formula col "SUM" :category))
 
 (defn sum-reqt-formula
   [col]
-  (fn [wave data col-ixs]
-    (let [cltr (ix-to-col (get col-ixs col))
-          rowids (row-ids wave #(and (= (:type %) :requirement) (= (:category %) (:category data)) (= (:subcategory %) (:subcategory data))))
-          begid (ix-to-row (apply min rowids))
-          endid (ix-to-row (apply max rowids))]
-      (str "SUM(" cltr begid ":" cltr endid ")"))))
+  (items-formula col "SUM" :requirement :subcategory :category))
 
 
 (defn infix-binary-formula
   [op arg1 arg2]
   (fn [wave data col-ixs]
-    (let [r (ix-to-row (:rownum data))
-          c1 (ix-to-col (get col-ixs arg1))
-          c2 (ix-to-col (get col-ixs arg2))]
+    (let [r (ix2r (:rownum data))
+          c1 (ix2c (get col-ixs arg1))
+          c2 (ix2c (get col-ixs arg2))]
     (str c1 r op c2 r))))
 
 
@@ -168,14 +163,6 @@
              {:type :subcategory :category c :subcategory s })
            subcats))))
 
-(defn cons-headers
-  "Adds header rows to the specified wave relation"
-  [w products]
-  (concat
-    [{:type :header :scores products} ; using scores to fit in render function
-     {:type :subheader :scores products}]
-    w))
-
 (defn pretty-map
   [m]
   (let [[f & r] (map (fn [[k v]] (str k " - " v)) m)]
@@ -192,7 +179,7 @@
          (conj-category-rows)
          (#(sort-by %2 %1) #(vector (:category %) (:subcategory %) (:reqtdesc %)))
          (#(concat %1 [{:type :totals :scores products}])); add line for totals
-         (cons-headers products)
+         (#(concat [{:type :header :scores products} {:type :subheader :scores products}] %1))
          (#(map-indexed (fn [ix r] (assoc r :rownum ix)) %1))))
 
 (defn merge-cols
@@ -233,51 +220,39 @@
 
 (defn prod-col-ixs
   [prod-num prod-cols other-cols]
-  (apply hash-map
-    (flatten
-      (map-indexed 
-        (fn [ix c]
-          [c (+ ix (count other-cols) (* (count prod-cols) prod-num))])
-      prod-cols))))
+  (into {}
+    (map-indexed 
+      (fn [ix c] [c (+ ix (count other-cols) (* (count prod-cols) prod-num))])
+    prod-cols)))
 
 (defn build-row
   [cols product-cols data {:keys [products wave row sheet]}]
-  (let [col-ixs (apply hash-map (flatten (map-indexed (comp reverse vector) cols)))
+  (let [col-ixs (into {} (map-indexed (comp vec reverse vector) cols))
         spec (get specs (:type data))
         prod-spec (:products spec)
         row-spec (dissoc spec :products)]
     (when spec
       (set-row-contents row row-spec col-ixs wave data)
-      (doseq [[pi p] (map-indexed vector products)]
+      (doseq [[pi p] (ix-seq products)]
         (set-row-contents row prod-spec 
           (merge (prod-col-ixs pi product-cols cols) col-ixs) 
           wave 
           (merge p data))))))
 
+
 (defn gen-xlsx
+  "Given a wave data structure, generate an xls workbook that computes scores and weights"
   [wave]
   (let [wb (org.apache.poi.xssf.usermodel.XSSFWorkbook.)
         sheet (.createSheet wb)
-        waverel (build-wave-relation wave)
-        styles (init-styles wb)
-        rowcounter (atom 0)]
-    (doseq [data waverel]
-      (let [row (.createRow sheet @rowcounter)]
-          (build-row 
-            shared-columns 
-            product-specific-columns 
-            data 
-            {:products (:products wave) :book wb :sheet sheet :row row :wave waverel}))
-      (swap! rowcounter + 1))
-    (.setColumnWidth sheet 0 (* 256 2))
-    (.setColumnWidth sheet 1 (* 256 10))
-    (.setColumnWidth sheet 2 (* 256 10))
-    (.setColumnWidth sheet 3 (* 256 50))
-    (.setColumnWidth sheet 4 (* 256 45))
-    (.setColumnWidth sheet 5 (* 256 8))
-    (.setColumnWidth sheet 6 (* 256 8))
-    (.setColumnWidth sheet 7 (* 256 8))
-    (.setColumnWidth sheet 8 (* 256 9))
+        waverel (build-wave-relation wave)]
+    (init-styles wb)
+    (doseq [[rowcounter data] (ix-seq waverel)]
+      (let [row (.createRow sheet rowcounter)]
+        (build-row shared-columns product-specific-columns data 
+          {:products (:products wave) :book wb :sheet sheet :row row :wave waverel})))
+    (doseq [[i c] (ix-seq [2 10 10 50 45 8 8 8 9])]
+      (.setColumnWidth sheet i (* 256 c)))
     (doseq [i (range 9 (+ 9 (count (:products wave))))]
       (.setColumnWidth sheet i (* 256 8)))
     wb))
