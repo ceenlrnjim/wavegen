504e7941e1a8da35746f05391e693f860e8aab36
Jim Kirkwood <jkirkwood@paychex.com>
2013-04-26 10:31:55 -0400

    updated poi dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.apache.poi/poi "3.7"]
+                 [org.apache.poi/poi "3.9"]
+                 [org.apache.poi/poi-ooxml "3.9"]
                  [log4j/log4j "1.2.16"]]
   :source-paths ["src" "/home/kirkwjl/projects/relations/clj/src"]
   :main wavegen.cli)
